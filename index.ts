7d7888304bfe158581601c57860d176ae52d0c85
amitknows <amitknowsyou@gmail.com>
2024-06-26 20:00:18 +0530

    add model schema

---
+++ index.ts
@@ -0,0 +1,8 @@
+let a:number = 2;
+
+function sum(num1:number
+    , num2:number){
+    return num1 + num2;
+}
+
+console.log(sum(a,2))
---
+++ src/model/User.Model.ts
@@ -0,0 +1,69 @@
+import mongoose, { Schema, Document } from "mongoose";
+
+// we are importing 'Document' only for type saftey (typescript)
+
+export interface Message extends Document {
+  content: string;
+  createdAt: Date;
+}
+
+const MessageSchema: Schema<Message> = new Schema({
+  content: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    required: true,
+    default: Date.now,
+  },
+});
+
+export interface User extends Document {
+  username: string;
+  email: string;
+  password: string;
+  verifyCode: string;
+  verifyCodeExpiry: Date;
+  isVerified: boolean;
+  isAcceptingMessage: boolean;
+  message: Message[];
+}
+
+const UserSchema: Schema<User> = new Schema({
+  username: {
+    type: String,
+    required: [true, "username is required"],
+    trim: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    unique: true,
+    match: [/.+\@.+\..+/, "Please enter a valid email"],
+  },
+  password: {
+    type: String,
+    required: [true, "Password is Required"],
+  },
+  verifyCode: {
+    type: String,
+    required: [true, "Verify code is required"],
+  },
+  verifyCodeExpiry: {
+    type: Date,
+    required: [true, "Verify code Expiry is required"],
+    default: Date.now,
+  },
+
+  isVerified: {
+    type: Boolean,
+    default: false,
+  },
+  isAcceptingMessage: {
+    type: Boolean,
+    deafult: true,
+  },
+  message:[MessageSchema],
+});
