9c39c536b30d8a86cf2c722cc33e2d99928cdfd9
amitknows <amitknowsyou@gmail.com>
2024-08-28 21:21:38 +0530

    AI , Login

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,22 +1,39 @@
 'use client'
 
 
-import { useSession, signIn, signOut } from "next-auth/react"
+// import { useSession, signIn, signOut } from "next-auth/react"
 
-export default function Component() {
-  const { data: session } = useSession()
-  if (session) {
-    return (
-      <>
-        Signed in as {session.user.email} <br />
-        <button onClick={() => signOut()}>Sign out</button>
-      </>
-    )
-  }
+// export default function Component() {
+//   const { data: session } = useSession()
+//   if (session) {
+//     return (
+//       <>
+//         Signed in as {session.user.email} <br />
+//         <button onClick={() => signOut()}>Sign out</button>
+//       </>
+//     )
+//   }
+//   return (
+//     <>
+//       Not signed in <br />
+//       <button className="bg-orange-500 px-5 py-3 m-2 rounded" onClick={() => signIn()}>Sign in</button>
+//     </>
+//   )
+// }
+
+
+
+
+
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import  * as Z  from "zod"
+import Link from "next/link"
+
+function page() {
   return (
-    <>
-      Not signed in <br />
-      <button className="bg-orange-500 px-5 py-3 m-2 rounded" onClick={() => signIn()}>Sign in</button>
-    </>
+    <div>page</div>
   )
 }
+
+export default page
